757437e5b07705b10a9b79436f0886bb5435ece1
Alexander Shinenkov <alex.shinenkov@gmail.com>
2025-02-09 23:48:59 +0300

    fix configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const mockHOST = env.MOCK_HOST
   const greenAPI = env.GREEN_API
 
   const proxyConfigLocal = {
-    '/mock': {
-      target: mockHOST,
-      changeOrigin: true,
-      rewrite: (path: string) => path.replace(/\/mock/, '')
-    },
     '/green': {
       target: greenAPI,
       changeOrigin: true,
